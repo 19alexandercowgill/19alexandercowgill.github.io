029704147745ff6b8313926aa44d83980c2d917c
19alexandercowgill <19alexandercowgill@st-ambrosecollege.org.uk>
2024-10-28 14:05:09 +0000

    Added CS pages section for CS. This is the first commit for that area and it is a WIP
    added a 404 page :)

---
+++ Computing/components.js
@@ -0,0 +1,50 @@
+function getNav() {
+    const navBar = document.getElementById("nav-top")
+    navBar.innerHTML = `<h1 id="title"><a id="title-link" href="/Computing/">Computing</a></h1>
+            <div id="links">
+                <li class="nav-item has-dropdown link">
+                    <a href="#">Component 1</a>
+                    <ul class="dropdown">
+                        <a href="/Computing/component-1/processors-io-storage/"><li class="overview-item">Key Features of Processors, I/O, and Storage Devices</li></a>
+                        <a href="/Computing/component-1/software-software-dev/"><li class="overview-item">Software and software development</li></a>
+                        <a href="/Computing/component-1/exchanging-data/"><li class="overview-item">Exchanging data</li></a>
+                        <a href="/Computing/component-1/data-algorithms/"><li class="overview-item">Data types, data structures and algorithms</li></a>
+                        <a href="/Computing/component-1/legal-moral-ethics/"><li class="overview-item">Legal, moral, cultural and ethical issues</li></a>
+                    <hr>
+                        <li class="dropdown-item"><a href="/Computing/component-1/">Component 1</a></li>
+                   
+                    </ul>
+                </li>
+                <li class="nav-item has-dropdown link">
+                    <a href="#">Component 2</a>
+                    <ul class="dropdown">
+                        <a href="/Computing/component-2/computer-thinking/"><li class="overview-item">Computational Thinking</li></a>
+                        <a href="/Computing/component-2/problems-programming/"><li class="overview-item">Problem Solving and Programming</li></a>
+                        <a href="/Computing/component-2/algorithms/"><li class="overview-item">Algorithms</li></a>
+                   <hr>
+                        <li class="dropdown-item"><a href="/Computing/component-2/">Component 2</a></li>
+                   
+                    </ul>
+                </li>
+                <li class="nav-item has-dropdown link">
+                    <a href="#">Component 3</a>
+                    <ul class="dropdown">
+                        <a href="/Computing/component-3/problem-analysis/"><li class="overview-item">Analysis of the problem</li></a>
+                        <a href="/Computing/component-3/solution-design/"><li class="overview-item">Design of the solution</li></a>
+                        <a href="/Computing/component-3/solution-dev/"><li class="overview-item">Developing the solution</li></a>
+                        <a href="/Computing/component-3/evaluation/"><li class="overview-item">Evaluation</li></a>
+                   <hr>
+                        <li class="dropdown-item"><a href="/Computing/component-3/">Component 3</a></li>
+                    </ul>
+                </li>
+            </div>`
+}
+
+function getFooter() {
+    const footer = document.getElementById("footer")
+    footer.innerHTML = ` <p>2024 Alexander Cowgill</p><p style="background-color:gray;border-radius:15px;padding:10px">This site is under construction. Not all pages will exist yet. <a href="/404.html">404</a></p><p>Found an issue? Report it at <a style="text-decoration:solid; text-decoration-line: underline;" href="https://github.com/19alexandercowgill/19alexandercowgill.github.io/issues/new">Github Issues<a>`
+}
+
+
+getNav()
+getFooter()
