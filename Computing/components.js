8a55f62c70e809b31677ebfca4217efe542d1122
19alexandercowgill <19alexandercowgill@st-ambrosecollege.org.uk>
2024-11-10 18:26:37 +0000

    fixed menu accessibility

--- Computing/components.js
+++ Computing/components.js
@@ -3,42 +3,41 @@ function getNav() {
     navBar.innerHTML = `<h1 id="title"><a id="title-link" href="/Computing/">Computing</a></h1>
             <div id="links">
             <menu>
-                <li tabindex="0" class="nav-item has-dropdown link">
-                    <a tabindex="0" href="#">Component 1</a>
-                    <ul class="dropdown">
-                        <a tabindex="0" href="/Computing/component-1/processors-io-storage/"><li class="overview-item">Key Features of Processors, I/O, and Storage Devices</li></a>
-                        <a tabindex="0" href="/Computing/component-1/software-software-dev/"><li class="overview-item">Software and software development</li></a>
-                        <a tabindex="0" href="/Computing/component-1/exchanging-data/"><li class="overview-item">Exchanging data</li></a>
-                        <a tabindex="0" href="/Computing/component-1/data-algorithms/"><li class="overview-item">Data types, data structures and algorithms</li></a>
-                        <a tabindex="0" href="/Computing/component-1/legal-moral-ethics/"><li class="overview-item">Legal, moral, cultural and ethical issues</li></a>
-                    <hr>
-                        <li class="dropdown-item"><a tabindex="0" href="/Computing/component-1/">Component 1</a></li>
-                   
-                    </ul>
-                </li>
-                <li tabindex="0" class="nav-item has-dropdown link">
-                    <a tabindex="0" href="#">Component 2</a>
-                    <ul class="dropdown">
-                        <a tabindex="0" href="/Computing/component-2/computer-thinking/"><li class="overview-item">Computational Thinking</li></a>
-                        <a tabindex="0" href="/Computing/component-2/problems-programming/"><li class="overview-item">Problem Solving and Programming</li></a>
-                        <a tabindex="0" href="/Computing/component-2/algorithms/"><li class="overview-item">Algorithms</li></a>
-                   <hr>
-                        <li class="dropdown-item"><a tabindex="0" href="/Computing/component-2/">Component 2</a></li>
-                   
-                    </ul>
-                </li>
-                <li tabindex="0" class="nav-item has-dropdown link">
-                    <a tabindex="0" href="#">Component 3</a>
-                    <ul class="dropdown">
-                        <a tabindex="0" href="/Computing/component-3/problem-analysis/"><li class="overview-item">Analysis of the problem</li></a>
-                        <a tabindex="0" href="/Computing/component-3/solution-design/"><li class="overview-item">Design of the solution</li></a>
-                        <a tabindex="0" href="/Computing/component-3/solution-dev/"><li class="overview-item">Developing the solution</li></a>
-                        <a tabindex="0" href="/Computing/component-3/evaluation/"><li class="overview-item">Evaluation</li></a>
-                   <hr>
-                        <li class="dropdown-item"><a tabindex="0" href="/Computing/component-3/">Component 3</a></li>
-                    </ul>
-                </li>
-                </menu>
+    <li tabindex="0" class="nav-item has-dropdown link">
+        <a tabindex="0" href="#">Component 1</a>
+        <ul class="dropdown">
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-1/processors-io-storage/">Key Features of Processors, I/O, and Storage Devices</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-1/software-software-dev/">Software and software development</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-1/exchanging-data/">Exchanging data</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-1/data-algorithms/">Data types, data structures and algorithms</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-1/legal-moral-ethics/">Legal, moral, cultural and ethical issues</a></li>
+            <hr>
+            <li class="dropdown-item"><a tabindex="0" href="/Computing/component-1/">Component 1</a></li>
+        </ul>
+    </li>
+    <li tabindex="0" class="nav-item has-dropdown link">
+        <a tabindex="0" href="#">Component 2</a>
+        <ul class="dropdown">
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-2/computer-thinking/">Computational Thinking</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-2/problems-programming/">Problem Solving and Programming</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-2/algorithms/">Algorithms</a></li>
+            <hr>
+            <li class="dropdown-item"><a tabindex="0" href="/Computing/component-2/">Component 2</a></li>
+        </ul>
+    </li>
+    <li tabindex="0" class="nav-item has-dropdown link">
+        <a tabindex="0" href="#">Component 3</a>
+        <ul class="dropdown">
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-3/problem-analysis/">Analysis of the problem</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-3/solution-design/">Design of the solution</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-3/solution-dev/">Developing the solution</a></li>
+            <li class="overview-item"><a tabindex="0" href="/Computing/component-3/evaluation/">Evaluation</a></li>
+            <hr>
+            <li class="dropdown-item"><a tabindex="0" href="/Computing/component-3/">Component 3</a></li>
+        </ul>
+    </li>
+</menu>
+
             </div>`
 }
 
