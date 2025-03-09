83a0e1f047b09612be76ade037e46938496e6a71
19alexandercowgill <19alexandercowgill@st-ambrosecollege.org.uk>
2025-03-09 17:50:55 +0000

    Modified website lots new layout, not all webpages linked anymore but that is for future commit

---
+++ navigation.js
@@ -0,0 +1,22 @@
+function injectLinks(){
+    const nav = document.getElementById("nav-head")
+   
+    nav.innerHTML = `
+     <ul>
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/Computing/">Computer Science</a></li>
+                <li><a href="#">Politics</a></li>
+                <li><a href="#">English Language</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Misc</a></li>
+            </ul>
+    `
+}
+function injectFooter(){
+    const foot = document.getElementById("footer-foot");
+    foot.innerHTML = `
+          &copy; 2025 Alexander Cowgill<br>This site is under construction. Your page probably isn't there. Sorry!
+    `
+}
+injectLinks();
+injectFooter();
